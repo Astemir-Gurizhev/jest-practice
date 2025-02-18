f62e5b59cf8f594cda5b566d3b58287a95d56887
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-18 21:21:27 +0300

    findBy, getBy, queryBy. An example with useEffect. Asynchronous code.

--- react-test/src/App.test.tsx
+++ react-test/src/App.test.tsx
@@ -1,13 +1,22 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
-	render(<App />)
-	const helloWorldElem = screen.getByText(/hello world/i)
-	const btn = screen.getByRole('button')
-	const input = screen.getByPlaceholderText(/input value/i)
-	expect(helloWorldElem).toBeInTheDocument()
-	expect(btn).toBeInTheDocument()
-	expect(input).toMatchSnapshot()
-	
+describe('react', () => {
+	test('renders learn react link', () => {
+		render(<App />)
+		const helloWorldElem = screen.getByText(/hello world/i)
+		const btn = screen.getByRole('button')
+		const input = screen.getByPlaceholderText(/input value/i)
+		expect(helloWorldElem).toBeInTheDocument()
+		expect(btn).toBeInTheDocument()
+		expect(input).toMatchSnapshot()
+		
+	})
+	test('renders learn react link', async () => {
+		render(<App/>)
+
+		const helloWorldElem = await screen.findByText(/data/i)
+		expect(helloWorldElem).toBeInTheDocument()
+		expect(helloWorldElem).toHaveStyle({color:'red'})
+	})
 })
--- react-test/src/App.tsx
+++ react-test/src/App.tsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState({})
+  useEffect(()=> {
+    setTimeout(()=> {
+      setData({})
+    },100)
+  }, [])
 	return <div className='App'>
+    {data && <div style={{color:'red'}}>data</div>}
     <h1>hello world</h1>
     <button>click me</button>
     <input placeholder='input value' type="text" />
