6464a93be8e4358df0948d5172962c48e21b2487
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-18 21:04:56 +0300

    Testing React applications

---
+++ react-test/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+	return <div className='App'>
+    <h1>hello world</h1>
+    <button>click me</button>
+    <input placeholder='input value' type="text" />
+  </div>
+}
+
+export default App
